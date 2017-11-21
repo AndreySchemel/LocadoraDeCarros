409a17208bf5bd88afc6e5d5d33c9bd0f3b2dd85
AndreySchemel <andrey.schemel@catolicasc.org.br>
2017-11-21 21:02:13 -0200

    Adicionar arquivos de projeto.

---
+++ LocadoraDeCarros/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LocadoraDeCarros.Startup))]
+namespace LocadoraDeCarros
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
